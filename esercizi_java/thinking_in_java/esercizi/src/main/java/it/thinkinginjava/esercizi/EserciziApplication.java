3d7a9bc444b19eeec2fc28f886a411ad217fb2f3
Luigi Rinaldi <l.rinaldi@mediaticaspa.it>
2023-05-04 16:59:26 +0200

    Add spring initializer configuration

---
+++ esercizi_java/thinking_in_java/esercizi/src/main/java/it/thinkinginjava/esercizi/EserciziApplication.java
@@ -0,0 +1,13 @@
+package it.thinkinginjava.esercizi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EserciziApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EserciziApplication.class, args);
+	}
+
+}
